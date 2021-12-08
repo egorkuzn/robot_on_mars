2b539fef88c4b9fdca7b34f254992b102154baa6
egorkuzn <egorkuzn2002@mail.ru>
2021-12-08 20:12:28 +0700

    sapper writing

--- sapper.h
+++ sapper.h
@@ -2,14 +2,25 @@
 
 #include <vector>
 #include "robot.h"
+#include "data.h"
+#include "item.h"
 
 namespace planet{
     class Supper: public Robot{
-        Supper: 
+        Supper(std::vector<vectorItems>& ground, data& server): Robot(ground, server){}
+        void refresh();
+        void on();
+        void off();
     };
      
     class vectorS: public std::vector<Supper>{
         public:
-
+            vectorS(std::vector<vectorItems>& ground, data& server) : 
+                ground(ground), server(server){}
+            void cmd();
+            void refresh();
+        private:
+            std::vector<vectorItems>& ground;
+            data& server;
     };
 }
--- surface.cpp
+++ surface.cpp
@@ -1,4 +1,20 @@
 #include "surface.h"
 
 namespace planet{
+    void Supper::on(){
+
+    }
+
+    void Supper::off(){
+
+    }
+
+    void vectorS::cmd(){
+        server.
+    }
+
+    void vectorS::refresh(){
+        for(Supper & elem : *this)
+            elem.refresh();
+    }
 }
