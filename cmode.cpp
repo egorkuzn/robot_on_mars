24875b42fe70bf2f946c98c9a48b00f70ae4018f
egorkuzn <egorkuzn2002@mail.ru>
2021-12-09 11:50:15 +0700

    cmode improving

--- cmode.cpp
+++ cmode.cpp
@@ -10,4 +10,24 @@ namespace planet{
         return;
     }
 
+    ScanMode::ScanMode(data& server){
+        server.send(x, y, id);
+        func(server);
+    }
+
+    void ScanMode::func(data& server){
+        if(isUnknownArea())
+            scan();        
+        else
+            goInRandWay();        
+    }
+
+    AutoMode::AutoMode(data& server){
+        if(isAnyAppleFound())
+            collectClosedApple();
+        else if(isUnknownArea())
+            scan();
+        else
+            goInRandWay();
+    }
 }
--- cmode.h
+++ cmode.h
@@ -11,24 +11,41 @@ namespace planet{
 
     class CMode{
         public:
-            virtual void func(data& server);
+            virtual void func(size_t x,
+                             size_t y,
+                             size_t id,
+                             data& server);
+        protected:
+            size_t x = 0;
+            size_t y = 0;
+            size_t id = 0;
+            data& server;
     };
 
     class ManualMode: public CMode{
         public:
             ManualMode(data& server);
-            void func(data& server) override;
+            void func(size_t x,
+                      size_t y,
+                      size_t id,
+                      data& server) override;
     };
 
     class AutoMode: public CMode{
         public:
             AutoMode(data& server);
-            void func(data& server) override;
+            void func(size_t x,
+                      size_t y,
+                      size_t id,
+                      data& server) override;
     };
 
     class ScanMode: public CMode{
         public:
             ScanMode(data& server);
-            void func(data& server) override;
+            void func(size_t x,
+                      size_t y,
+                      size_t id,
+                      data& server) override;
     };
 }
